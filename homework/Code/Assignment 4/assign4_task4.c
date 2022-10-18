061ccc404f2128a806a715bddc1b4a9f1e794bb8
saraprettyman <112831068+saraprettyman@users.noreply.github.com>
2022-10-17 23:15:10 -0600

    Update assign4_task4.c

--- homework/Code/Assignment 4/assign4_task4.c
+++ homework/Code/Assignment 4/assign4_task4.c
@@ -4,7 +4,7 @@
 
 double absolute_error(double u, double v) {
     double abs_error = fabs(u - v);
-    printf("%f\n", abs_error);
+    printf("Absolute error is %f\n", abs_error);
     return 0;
 }
 
@@ -13,7 +13,7 @@ double relative_error(double u, double v) {
         return LLONG_MAX;
     }
     double rel_error = (fabs(u - v)/u);
-    printf("%f\n", rel_error);
+    printf("Relative error is %f\n", rel_error);
     return 0;
 }
 
@@ -22,7 +22,7 @@ int double_precision(double u, double v) {
     double dpower = log10l(diff);
     double upower = log10l(u);
     double d_precision = (floor(upower - dpower));
-    printf("%f\n", d_precision);
+    printf("Double precision is %f\n", d_precision);
     return 0;
 }
 
@@ -31,7 +31,7 @@ int float_precision(float u, float v) {
     float dpower = log10f(diff);
     float upower = log10f(u);
     float f_precision = (floor(upower - dpower));
-    printf("%f\n", f_precision);
+    printf("Float precision is %f\n", f_precision);
     return 0;
 }
 
