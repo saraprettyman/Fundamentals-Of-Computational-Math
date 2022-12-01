b077004dec90cb4a5cb8b827986912c07ff59ed8
saraprettyman <112831068+saraprettyman@users.noreply.github.com>
2022-11-30 19:19:36 -0700

    Update assign7_task4.c

--- homework/Code/Assignment_7/assign7_task4.c
+++ homework/Code/Assignment_7/assign7_task4.c
@@ -6,12 +6,12 @@
 // Initialization of Variables 
 double matrix1[3][3] = {{1.0,2.0,3.0}, {4.0,5.0,6.0}, {7.0,8.0,9.0}};
 double matrix2[3][3] = {{9.0,8.0,7.0}, {6.0,5.0,4.0}, {3.0,2.0,1.0}};
-int m = 3;
-int n = 3;
+int m = 3; // Total number of rows
+int n = 3; // Total number of columns
 
 
 // Method
-static double **hadm(int m, int n, double a[m][n], double b[m][n]){
+static double **hadamardM(int m, int n, double a[m][n], double b[m][n]){
     double **c;
     c = malloc(sizeof(double*)*m);
     printf("    \n");
@@ -33,7 +33,7 @@ static double **hadm(int m, int n, double a[m][n], double b[m][n]){
 // Implementation Example
 int main(){
     double** r1;
-    r1 = hadm(m, n, matrix1, matrix2);
+    r1 = hadamardM(m, n, matrix1, matrix2);
     for(int i=0; i < m; i++){
         for(int j=0; j < n; j++){
             printf("%.2f    ", r1[i][j]);
